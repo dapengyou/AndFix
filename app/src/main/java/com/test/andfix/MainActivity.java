2d24ddfab9dec25e3d5482cc027a4d703c4f7c50
dapengyou <ladyztt@sina.com>
2018-05-23 18:24:36 +0800

    使用代码打包，，生成有bug的apk

--- app/src/main/java/com/test/andfix/MainActivity.java
+++ app/src/main/java/com/test/andfix/MainActivity.java
@@ -5,11 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.test.andfix.andfix.AndFixPatchManager;
+import com.test.andfix.util.Utils;
+
+import java.io.File;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String FILE_END = ".apatch";
+    private String mFileDir;//存储文件路径
+
     @BindView(R.id.bt_getBug)
     Button mBtGetBug;
     @BindView(R.id.bt_fixBug)
@@ -20,17 +28,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        initDir();
     }
+
+    /**
+     * 初始化文件
+     */
+    private void initDir() {
+        //设置文件路径
+        mFileDir = getExternalCacheDir().getAbsolutePath() + "/apatch";
+        //创建文件夹
+        File file = new File(mFileDir);
+
+        //当文件为null或者不存在时 创建文件
+        if (file == null || file.exists()) {
+            file.mkdir();
+        }
+    }
+
 
     @OnClick({R.id.bt_fixBug, R.id.bt_getBug})
     public void onClicked(View view) {
         switch (view.getId()) {
             case R.id.bt_getBug:
-
+                creatBug();
                 break;
             case R.id.bt_fixBug:
-
+                fixBug();
                 break;
         }
     }
+
+    /**
+     * 创建bug
+     */
+    private void creatBug() {
+        Utils.printLog();
+    }
+
+    /**
+     * 修复bug
+     */
+    private void fixBug() {
+        AndFixPatchManager.getInstance().addPatch(getPatchName());
+    }
+
+    /**
+     * 构建patch文件名
+     *
+     * @return
+     */
+    private String getPatchName() {
+        return mFileDir.concat("test").concat(FILE_END);
+    }
+
+
 }
--- app/src/main/java/com/test/andfix/util/Utils.java
+++ app/src/main/java/com/test/andfix/util/Utils.java
@@ -3,6 +3,7 @@ package com.test.andfix.util;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.util.Log;
 
 /**
  * Created by lady_zhou on 2018/5/23.
@@ -25,5 +26,10 @@ public class Utils {
             e.printStackTrace();
         }
         return versionName;
+    }
+
+    public static void printLog() {
+        String error = null;
+        Log.d("utils", error);//log输出的字符串为空会报异常
     }
 }
